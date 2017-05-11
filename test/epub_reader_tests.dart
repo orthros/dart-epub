75ab74d1bfb4eee5cab9f57659db933baa944b93
Colin <colinnlsn@gmail.com>
2017-05-11 17:35:23 -0500

    Updated Tests with new Epub
    Added a new epub with proper formatting on cover page.
    Updated unit tests accordingly

--- test/epub_reader_tests.dart
+++ test/epub_reader_tests.dart
@@ -5,21 +5,21 @@ import 'package:epub/epub.dart';
 import 'dart:io' as IO;
 import 'package:path/path.dart' as path;
 
-main() async {  
+main() async {
+  
+  String fileName = "hittelOnGoldMines.epub";
+  String fullPath = path.join(IO.Directory.current.path, "test", fileName);
+  
   await test("Test Epub Ref", () async {  
-   String fileName = "alice.epub";
-   String fullPath = path.join(IO.Directory.current.path, "test", fileName);
    EpubBookRef epubRef = await EpubReader.OpenBookAsync(fullPath);
    
-   expect(epubRef.Author, equals("Lewis Carroll"));  
-   expect(epubRef.Title, equals("Alice's Adventures in Wonderland"));
+   expect(epubRef.Author, equals("John S. Hittell"));  
+   expect(epubRef.Title, equals("Hittel on Gold Mines and Mining"));
   });
   await test("Test Epub Read", () async {
-    String fileName = "alice.epub";
-    String fullPath = path.join(IO.Directory.current.path, "test", fileName);
     EpubBook epubRef = await EpubReader.ReadBookAsync(fullPath);
    
-    expect(epubRef.Author, equals("Lewis Carroll"));  
-    expect(epubRef.Title, equals("Alice's Adventures in Wonderland"));
+    expect(epubRef.Author, equals("John S. Hittell"));  
+    expect(epubRef.Title, equals("Hittel on Gold Mines and Mining"));
   });
  }
