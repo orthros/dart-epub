5f5ba9b51acadce41b3e516e80746fb8549df745
Colin <colinnlsn@gmail.com>
2017-05-11 10:33:10 -0500

    Updated Travis Script
    Updated script with updated file names

--- tool/travis.sh
+++ tool/travis.sh
@@ -5,8 +5,9 @@ set -e
 
 # Analyze the code.
 dartanalyzer --fatal-warnings \
-  lib/dartpub.dart \
+  lib/epub.dart \
   lib/src/epubReader.dart
 
 # Run the tests.
 dart -c test/enum_string_test.dart
+dart -c test/epub_reader_tests.dart
