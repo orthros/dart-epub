3cf604b88c104647575744585d0886a76fe083a7
orthros <colinnlsn@gmail.com>
2017-05-11 08:33:25 -0500

    Added Test for Epub read
    Added a test to read the epub into memory.

--- test/epub_reader_tests.dart
+++ test/epub_reader_tests.dart
@@ -6,12 +6,20 @@ import 'dart:io' as IO;
 import 'package:path/path.dart' as path;
 
 main() async {  
-  await test("Test Epub", () async {  
+  await test("Test Epub Ref", () async {  
    String fileName = "Alice.epub";
    String fullPath = path.join(IO.Directory.current.path, fileName);
    EpubBookRef epubRef = await EpubReader.OpenBookAsync(fullPath);
    
    expect(epubRef.Author, equals("Lewis Carroll"));  
    expect(epubRef.Title, equals("Alice's Adventures in Wonderland"));
-  });  
+  });
+  await test("Test Epub Read", () async {
+    String fileName = "Alice.epub";
+    String fullPath = path.join(IO.Directory.current.path, fileName);
+    EpubBook epubRef = await EpubReader.ReadBookAsync(fullPath);
+   
+    expect(epubRef.Author, equals("Lewis Carroll"));  
+    expect(epubRef.Title, equals("Alice's Adventures in Wonderland"));
+  });
  }
