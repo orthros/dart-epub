8108173df1418a913a9a69c51feb77c590d9c103
Colin <colinnlsn@gmail.com>
2017-05-11 16:24:28 -0500

    Travis upgrades
    Made warnings not errors.
    Reintroduced unit tests

--- tool/travis.sh
+++ tool/travis.sh
@@ -4,10 +4,10 @@
 set -e
 
 # Analyze the code.
-dartanalyzer --fatal-warnings \
+dartanalyzer \
   lib/epub.dart \
   lib/src/epubReader.dart
 
 # Run the tests.
-#dart -c test/enum_string_test.dart
-#dart -c test/epub_reader_tests.dart
+dart -c test/enum_string_test.dart
+dart -c test/epub_reader_tests.dart
