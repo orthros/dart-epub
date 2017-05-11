29a305e9aced2315ec4ab86044595f7db4c8d977
orthros <colinnlsn@gmail.com>
2017-05-11 06:21:23 -0500

    Updates to Enum String Test
    Fixed broken import, and added a few more tests

--- test/enum_string_test.dart
+++ test/enum_string_test.dart
@@ -1,11 +1,20 @@
 library epubtest;
 
 import 'package:test/test.dart';
-import 'package:epub/epub.dart';
+import 'package:dartpub/epub.dart';
 
 main() {  
-  test("Enum", () {  
+  test("Enum One", () {  
    expect( new EnumFromString<Simple>().get("ONE"), equals(Simple.ONE));  
+  });  
+  test("Enum Two", () {  
+   expect( new EnumFromString<Simple>().get("TWO"), equals(Simple.TWO));  
+  });  
+  test("Enum One", () {  
+   expect( new EnumFromString<Simple>().get("THREE"), equals(Simple.THREE));  
+  });  
+  test("Enum One Lower Case", () {  
+   expect( new EnumFromString<Simple>().get("one"), equals(Simple.ONE));  
   });  
  }  
 
