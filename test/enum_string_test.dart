6c38b34a83fc5a3578199cee09eb061587d26d58
orthros <colinnlsn@gmail.com>
2017-05-12 06:53:25 -0500

    Formated Test Files
    Formatted Test files to be in alignment with dart style guides

--- test/enum_string_test.dart
+++ test/enum_string_test.dart
@@ -1,25 +1,22 @@
 library epubtest;
 
 import 'package:test/test.dart';
+
 import 'package:epub/epub.dart';
 
-main() {  
-  test("Enum One", () {  
-   expect( new EnumFromString<Simple>().get("ONE"), equals(Simple.ONE));  
-  });  
-  test("Enum Two", () {  
-   expect( new EnumFromString<Simple>().get("TWO"), equals(Simple.TWO));  
-  });  
-  test("Enum One", () {  
-   expect( new EnumFromString<Simple>().get("THREE"), equals(Simple.THREE));  
-  });  
-  test("Enum One Lower Case", () {  
-   expect( new EnumFromString<Simple>().get("one"), equals(Simple.ONE));  
-  });  
- }  
+main() {
+  test("Enum One", () {
+    expect(new EnumFromString<Simple>().get("ONE"), equals(Simple.ONE));
+  });
+  test("Enum Two", () {
+    expect(new EnumFromString<Simple>().get("TWO"), equals(Simple.TWO));
+  });
+  test("Enum One", () {
+    expect(new EnumFromString<Simple>().get("THREE"), equals(Simple.THREE));
+  });
+  test("Enum One Lower Case", () {
+    expect(new EnumFromString<Simple>().get("one"), equals(Simple.ONE));
+  });
+}
 
- enum Simple {
-   ONE,
-   TWO,
-   THREE
- }
+enum Simple { ONE, TWO, THREE }
--- test/epub_reader_tests.dart
+++ test/epub_reader_tests.dart
@@ -1,25 +1,26 @@
 library epubreadertest;
 
-import 'package:test/test.dart';
-import 'package:epub/epub.dart';
-import 'dart:io' as IO;
+import 'dart:io' as io;
+
 import 'package:path/path.dart' as path;
+import 'package:test/test.dart';
+
+import 'package:epub/epub.dart';
 
 main() async {
-  
   String fileName = "hittelOnGoldMines.epub";
-  String fullPath = path.join(IO.Directory.current.path, "test", fileName);
-  
-  await test("Test Epub Ref", () async {  
-   EpubBookRef epubRef = await EpubReader.OpenBookAsync(fullPath);
-   
-   expect(epubRef.Author, equals("John S. Hittell"));  
-   expect(epubRef.Title, equals("Hittel on Gold Mines and Mining"));
+  String fullPath = path.join(io.Directory.current.path, "test", fileName);
+
+  await test("Test Epub Ref", () async {
+    EpubBookRef epubRef = await EpubReader.OpenBookAsync(fullPath);
+
+    expect(epubRef.Author, equals("John S. Hittell"));
+    expect(epubRef.Title, equals("Hittel on Gold Mines and Mining"));
   });
   await test("Test Epub Read", () async {
     EpubBook epubRef = await EpubReader.ReadBookAsync(fullPath);
-   
-    expect(epubRef.Author, equals("John S. Hittell"));  
+
+    expect(epubRef.Author, equals("John S. Hittell"));
     expect(epubRef.Title, equals("Hittel on Gold Mines and Mining"));
   });
- }
+}
