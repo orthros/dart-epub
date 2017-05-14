48c33b426b4037506e7282b89c470659b605445b
Colin <colinnlsn@gmail.com>
2017-05-14 13:23:58 -0500

    Fixed Null Reference in BookCoverReader
    EpubMetadataMeta can have a Name that is null. Adding a check for this.
    Also bumping pubspec version

--- lib/src/readers/book_cover_reader.dart
+++ lib/src/readers/book_cover_reader.dart
@@ -12,15 +12,16 @@ class BookCoverReader {
     List<EpubMetadataMeta> metaItems =
         bookRef.Schema.Package.Metadata.MetaItems;
     if (metaItems == null || metaItems.length == 0) return null;
-    
+
     EpubMetadataMeta coverMetaItem = metaItems.firstWhere(
-        (EpubMetadataMeta metaItem) => metaItem.Name.toLowerCase() == "cover",
+        (EpubMetadataMeta metaItem) =>
+            metaItem.Name != null && metaItem.Name.toLowerCase() == "cover",
         orElse: () => null);
     if (coverMetaItem == null) return null;
     if (coverMetaItem.Content == null || coverMetaItem.Content.isEmpty)
       throw new Exception(
           "Incorrect EPUB metadata: cover item content is missing.");
-    
+
     EpubManifestItem coverManifestItem = bookRef.Schema.Package.Manifest.Items
         .firstWhere(
             (EpubManifestItem manifestItem) =>
@@ -30,12 +31,12 @@ class BookCoverReader {
     if (coverManifestItem == null)
       throw new Exception(
           "Incorrect EPUB manifest: item with ID = \"${coverMetaItem.Content}\" is missing.");
-    
+
     EpubByteContentFileRef coverImageContentFileRef;
     if (!bookRef.Content.Images.containsKey(coverManifestItem.Href))
       throw new Exception(
           "Incorrect EPUB manifest: item with href = \"${coverManifestItem.Href}\" is missing.");
-    
+
     coverImageContentFileRef = bookRef.Content.Images[coverManifestItem.Href];
     List<int> coverImageContent =
         await coverImageContentFileRef.ReadContentAsBytesAsync();
