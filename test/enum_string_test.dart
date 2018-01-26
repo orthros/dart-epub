a92764560d53bfff8cae195deef7d17b2f85df5d
Mostafa Ayesh <MostafaAyesh@outlook.com>
2018-01-25 22:46:02 -0500

    Change EnumFromString
    Changed EnumFromString Class to work without the need of dart:mirrors

--- test/enum_string_test.dart
+++ test/enum_string_test.dart
@@ -6,16 +6,16 @@ import 'package:epub/epub.dart';
 
 main() {
   test("Enum One", () {
-    expect(new EnumFromString<Simple>().get("ONE"), equals(Simple.ONE));
+    expect(new EnumFromString<Simple>(Simple.values).get("ONE"), equals(Simple.ONE));
   });
   test("Enum Two", () {
-    expect(new EnumFromString<Simple>().get("TWO"), equals(Simple.TWO));
+    expect(new EnumFromString<Simple>(Simple.values).get("TWO"), equals(Simple.TWO));
   });
   test("Enum One", () {
-    expect(new EnumFromString<Simple>().get("THREE"), equals(Simple.THREE));
+    expect(new EnumFromString<Simple>(Simple.values).get("THREE"), equals(Simple.THREE));
   });
   test("Enum One Lower Case", () {
-    expect(new EnumFromString<Simple>().get("one"), equals(Simple.ONE));
+    expect(new EnumFromString<Simple>(Simple.values).get("one"), equals(Simple.ONE));
   });
 }
 
