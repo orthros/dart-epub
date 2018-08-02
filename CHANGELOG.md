5376d149474fc902946b13b952e925a3399a136b
orthros <colinnlsn@gmail.com>
2018-08-01 19:42:09 -0700

    Updated tests and .travis.yml. Formatted documents

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,8 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+- Updates to Travis
+
 ## [1.3.0] - 2018-08-01
 ### Added
 - Package now supports Dart 2!
--- test/enum_string_test.dart
+++ test/enum_string_test.dart
@@ -6,16 +6,20 @@ import 'package:epub/epub.dart';
 
 main() {
   test("Enum One", () {
-    expect(new EnumFromString<Simple>(Simple.values).get("ONE"), equals(Simple.ONE));
+    expect(new EnumFromString<Simple>(Simple.values).get("ONE"),
+        equals(Simple.ONE));
   });
   test("Enum Two", () {
-    expect(new EnumFromString<Simple>(Simple.values).get("TWO"), equals(Simple.TWO));
+    expect(new EnumFromString<Simple>(Simple.values).get("TWO"),
+        equals(Simple.TWO));
   });
   test("Enum One", () {
-    expect(new EnumFromString<Simple>(Simple.values).get("THREE"), equals(Simple.THREE));
+    expect(new EnumFromString<Simple>(Simple.values).get("THREE"),
+        equals(Simple.THREE));
   });
   test("Enum One Lower Case", () {
-    expect(new EnumFromString<Simple>(Simple.values).get("one"), equals(Simple.ONE));
+    expect(new EnumFromString<Simple>(Simple.values).get("one"),
+        equals(Simple.ONE));
   });
 }
 
