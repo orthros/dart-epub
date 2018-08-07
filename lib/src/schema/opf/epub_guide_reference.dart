59a8cf2c855374271efdc7451d9dfeca63c67607
orthros <colinnlsn@gmail.com>
2018-08-06 19:24:02 -0700

    EpubGuideReference overriden == and hashCode

--- lib/src/schema/opf/epub_guide_reference.dart
+++ lib/src/schema/opf/epub_guide_reference.dart
@@ -1,3 +1,5 @@
+import 'package:quiver/core.dart';
+
 class EpubGuideReference {
   String Type;
   String Title;
@@ -6,4 +8,17 @@ class EpubGuideReference {
   String toString() {
     return "Type: ${Type}, Href: ${Href}";
   }
+
+  bool operator ==(other) {
+    var otherAs = other as EpubGuideReference;
+    if (otherAs == null) {
+      return false;
+    }
+
+    return Type == otherAs.Type &&
+        Title == otherAs.Title &&
+        Href == otherAs.Href;
+  }
+
+  int get hashCode => hash3(Type.hashCode, Title.hashCode, Href.hashCode);
 }
