fa8a4ebcf21b16b8f8a43baf3e58b23b201c925f
orthros <colinnlsn@gmail.com>
2018-08-06 19:34:17 -0700

    Formatted src/readers directory

--- lib/src/readers/schema_reader.dart
+++ lib/src/readers/schema_reader.dart
@@ -2,13 +2,13 @@ import 'dart:async';
 
 import 'package:archive/archive.dart';
 
-import 'navigation_reader.dart';
-import 'package_reader.dart';
-import 'root_file_path_reader.dart';
 import '../entities/epub_schema.dart';
 import '../schema/navigation/epub_navigation.dart';
 import '../schema/opf/epub_package.dart';
 import '../utils/zip_path_utils.dart';
+import 'navigation_reader.dart';
+import 'package_reader.dart';
+import 'root_file_path_reader.dart';
 
 class SchemaReader {
   static Future<EpubSchema> readSchema(Archive epubArchive) async {
