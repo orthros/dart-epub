a42c6dc760de44dd2a8183e46ff5ca9125a10f16
orthros <colinnlsn@gmail.com>
2018-08-07 17:59:06 -0700

    Fixed bug in EpubGuide's hashCode and added associated test

--- lib/src/schema/opf/epub_guide.dart
+++ lib/src/schema/opf/epub_guide.dart
@@ -1,12 +1,21 @@
 import 'package:quiver/collection.dart' as collections;
+import 'package:quiver/core.dart';
 
 import 'epub_guide_reference.dart';
 
 class EpubGuide {
   List<EpubGuideReference> Items;
 
+  EpubGuide() {
+    Items = new List<EpubGuideReference>();
+  }
+
   @override
-  int get hashCode => Items.hashCode;
+  int get hashCode {
+    var objects = [];
+    objects.addAll(Items.map((item) => item.hashCode));
+    return hashObjects(objects);
+  }
 
   bool operator ==(other) {
     var otherAs = other as EpubGuide;
