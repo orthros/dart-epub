7c694ee42bc2cf3b99523fd67a30dd27117ee0df
orthros <colinnlsn@gmail.com>
2018-08-07 19:09:19 -0700

    Fixed bug in EpubNavigationTarget hashCode and added associated test

--- lib/src/schema/navigation/epub_navigation_target.dart
+++ lib/src/schema/navigation/epub_navigation_target.dart
@@ -13,14 +13,16 @@ class EpubNavigationTarget {
   EpubNavigationContent Content;
 
   @override
-  int get hashCode => hashObjects([
-        Id.hashCode,
-        Class.hashCode,
-        Value.hashCode,
-        PlayOrder.hashCode,
-        NavigationLabels.hashCode,
-        Content.hashCode
-      ]);
+  int get hashCode {
+    var objects = []
+      ..add(Id.hashCode)
+      ..add(Class.hashCode)
+      ..add(Value.hashCode)
+      ..add(PlayOrder.hashCode)
+      ..add(Content.hashCode)
+      ..addAll(NavigationLabels.map((label) => label.hashCode));
+    return hashObjects(objects);
+  }
 
   bool operator ==(other) {
     var otherAs = other as EpubNavigationTarget;
