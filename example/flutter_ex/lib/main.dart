633b35d1d046273c8ec285eb9a3d9b191e52020f
Colin Nelson <colinnlsn@gmail.com>
2019-05-16 08:47:51 -0700

    Updated flutter example with a slightly more realistic example

--- example/flutter_ex/lib/main.dart
+++ example/flutter_ex/lib/main.dart
@@ -2,47 +2,138 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:epub/epub.dart' as epub;
 
-void main() => runApp(MyApp(post: fetchBook()));
+void main() => runApp(EpubWidget());
 
-class MyApp extends StatelessWidget {
-  final Future<epub.EpubBookRef> post;
+class EpubWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new EpubState();
+  }
+}
 
-  MyApp({Key key, this.post}) : super(key: key);
+class EpubState extends State<EpubWidget> {
+  Future<epub.EpubBookRef> book;
+
+  final _urlController = TextEditingController();
+
+  void fetchBookButton() {
+    setState(() {
+      book = fetchBook(_urlController.text);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fetch Epub Example',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Fetch Epub Example'),
-        ),
-        body: Center(
-          child: FutureBuilder<epub.EpubBookRef>(
-            future: post,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Text(snapshot.data.Title);
-              } else if (snapshot.hasError) {
-                return Text("${snapshot.error}");
-              }
-              // By default, show a loading spinner
-              return CircularProgressIndicator();
-            },
-          ),
-        ),
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        title: "Fetch Epub Example",
+        home: new Material(
+            child: new Container(
+                padding: const EdgeInsets.all(30.0),
+                color: Colors.white,
+                child: new Container(
+                  child: new Center(
+                      child: new Column(children: [
+                    new Padding(padding: EdgeInsets.only(top: 140.0)),
+                    new Text(
+                      'Epub Inspector',
+                      style: new TextStyle(fontSize: 25.0),
+                    ),
+                    new Padding(padding: EdgeInsets.only(top: 50.0)),
+                    new Text(
+                      'Enter the Url of an Epub to view some of it\'s metadata.',
+                      style: new TextStyle(fontSize: 18.0),
+                      textAlign: TextAlign.center,
+                    ),
+                    new Padding(padding: EdgeInsets.only(top: 20.0)),
+                    new TextFormField(
+                      decoration: new InputDecoration(
+                        labelText: "Enter Url",
+                        fillColor: Colors.white,
+                        border: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(25.0),
+                          borderSide: new BorderSide(),
+                        ),
+                      ),
+                      validator: (val) {
+                        if (val.length == 0) {
+                          return "Url cannot be empty";
+                        } else {
+                          return null;
+                        }
+                      },
+                      controller: _urlController,
+                      keyboardType: TextInputType.url,
+                      style: new TextStyle(
+                        fontFamily: "Poppins",
+                      ),
+                    ),
+                    new Padding(
+                      padding: EdgeInsets.only(top: 20.0),
+                    ),
+                    new RaisedButton(
+                      padding: const EdgeInsets.all(8.0),
+                      textColor: Colors.white,
+                      color: Colors.blue,
+                      onPressed: fetchBookButton,
+                      child: new Text("Inspect Book"),
+                    ),
+                    new Padding(padding: EdgeInsets.only(top: 25.0)),
+                    Center(
+                      child: FutureBuilder<epub.EpubBookRef>(
+                        future: book,
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            return new Material(
+                                color: Colors.white,
+                                child: buildEpubWidget(snapshot.data));
+                          } else if (snapshot.hasError) {
+                            return Text("${snapshot.error}");
+                          }
+                          // By default, show a loading spinner
+                          // return CircularProgressIndicator();
+
+                          // By default, show just empty.
+                          return Container();
+                        },
+                      ),
+                    ),
+                  ])),
+                ))));
   }
 }
 
-Future<epub.EpubBookRef> fetchBook() async {
+Widget buildEpubWidget(epub.EpubBookRef book) {
+  return Container(
+      child: new Column(
+    children: <Widget>[
+      Text(
+        "Title",
+        style: TextStyle(fontSize: 20.0),
+      ),
+      Text(
+        book.Title,
+        style: TextStyle(fontSize: 15.0),
+      ),
+      new Padding(
+        padding: EdgeInsets.only(top: 15.0),
+      ),
+      Text(
+        "Author",
+        style: TextStyle(fontSize: 20.0),
+      ),
+      Text(
+        book.Author,
+        style: TextStyle(fontSize: 15.0),
+      )
+    ],
+  ));
+}
+
+// Needs a url to a valid url to an epub like 'https://www.gutenberg.org/ebooks/11.epub.images'
+Future<epub.EpubBookRef> fetchBook(String url) async {
   // Hard coded to Alice Adventures In Wonderland in Project Gutenberb
-  final response =
-      await http.get('https://www.gutenberg.org/ebooks/11.epub.images');
+  final response = await http.get(url);
 
   if (response.statusCode == 200) {
     // If server returns an OK response, parse the EPUB
