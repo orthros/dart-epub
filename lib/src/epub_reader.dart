0d3fc7817cf85fbd83b6cb8887fccafa485b8b54
Kristen McWilliam <merritt_public@outlook.com>
2020-09-20 15:42:41 -0400

    Add doc comment to EpubReader and openBook()

--- lib/src/epub_reader.dart
+++ lib/src/epub_reader.dart
@@ -18,8 +18,38 @@ import 'ref_entities/epub_content_ref.dart';
 import 'ref_entities/epub_text_content_file_ref.dart';
 import 'schema/opf/epub_metadata_creator.dart';
 
+/// A class that provides the primary interface to read Epub files.
+///
+/// To open an Epub and load all data at once use the [readBook()] method.
+///
+/// To open an Epub and load only basic metadata use the [openBook()] method.
+/// This is a good option to quickly load text-based metadata, while leaving the
+/// heavier lifting of loading images and main content for subsequent operations.
+///
+/// ## Example
+/// ```dart
+/// // Read the basic metadata.
+/// EpubBookRef epub = await EpubReader.openBook(epubFileBytes);
+/// // Extract values of interest.
+/// String title = epub.Title;
+/// String author = epub.Author;
+/// var metadata = epub.Schema.Package.Metadata;
+/// String genres = metadata.Subjects.join(', ');
+/// ```
 class EpubReader {
-  /// Opens the book asynchronously without reading its content. Holds the handle to the EPUB file.
+  /// Loads basics metadata.
+  ///
+  /// Opens the book asynchronously without reading its main content.
+  /// Holds the handle to the EPUB file.
+  ///
+  /// Argument [bytes] should be the bytes of
+  /// the epub file you have loaded with something like the [dart:io] package's
+  /// [readAsBytes()].
+  ///
+  /// This is a fast and convenient way to get the most important information
+  /// about the book, notably the [Title], [Author] and [AuthorList].
+  /// Additional information is loaded in the [Schema] property such as the
+  /// Epub version, Publishers, Languages and more.
   static Future<EpubBookRef> openBook(List<int> bytes) async {
     Archive epubArchive = ZipDecoder().decodeBytes(bytes);
 
